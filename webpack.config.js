f6d60496cce694c04f7d83a980fe43a265c24af9
Ricardo Mayerhofer <ricardo_mayerhofer@epam.com>
2018-05-23 01:09:45 +0200

    Task2: Configured dev and prod build scripts using env var.

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,36 @@
 var path = require('path');
 
-module.exports = {
-    entry: ['./src/index.js'],
-    mode: 'development', // 'production', 'none'
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'bundle.js'
-    },
-    watch: true,
-    devServer: {
-        contentBase: path.join(__dirname, 'build'),
-        compress: true,
-        port: 3000
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                include: path.join(__dirname, 'src'),
-                loader: 'babel-loader',
-                query: {
-                    presets: ['env', 'react']
+module.exports = env => {
+
+    return {
+        entry: ['./src/index.js'],
+        mode: env.devOrProdMode, // 'development', 'production' or 'none'
+        output: {
+            path: path.join(__dirname, 'build'),
+            filename: 'bundle.js'
+        },
+        watch: true,
+        devServer: {
+            contentBase: path.join(__dirname, 'build'),
+            compress: true,
+            port: 3000
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?$/,
+                    exclude: /node_modules/,
+                    include: path.join(__dirname, 'src'),
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['env', 'react']
+                    }
+                },
+                {
+                    test: /\.css$/,
+                    loaders: ['style-loader!css-loader']
                 }
-            },
-            {
-                test: /\.css$/,
-                loaders: ['style-loader!css-loader']
-            }
-        ]
+            ]
+        }
     }
 }
