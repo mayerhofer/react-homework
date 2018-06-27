a9d3527218069d315feb3ed72f71c2c31d4cc864
Ricardo Mayerhofer <ricardo.mayerhofer@gmail.com>
2018-06-27 23:35:30 +0200

    Fixes for ErrorBoundary.

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -4,12 +4,12 @@ import ReactDOM from 'react-dom';
 class ErrorBoundary extends React.Component {
     constructor(props) {
       super(props);
-      this.state = { hasError: false };
+      this.state = { hasError: false, error: undefined, errorInfo: undefined };
     }
   
     componentDidCatch(error, info) {
       // Display fallback UI
-      this.setState({ hasError: true });
+      this.setState({ hasError: true, error: error, errorInfo: info });
       // You can also log the error to an error reporting service
       //logErrorToMyService(error, info);
     }
@@ -23,7 +23,7 @@ class ErrorBoundary extends React.Component {
                 <details style={{ whiteSpace: 'pre-wrap' }}>
                     {this.state.error && this.state.error.toString()}
                     <br />
-                    {this.state.errorInfo.componentStack}
+                    {this.state.errorInfo && this.state.errorInfo.componentStack}
                 </details>
             </div>
         );
