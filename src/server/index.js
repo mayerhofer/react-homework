f981d9ee453fb8643351abdfd9628d4cbd1a9004
Ricardo Mayerhofer <ricardo_mayerhofer@epam.com>
2018-06-30 15:04:53 +0200

    Installed express and created server index.js

---
+++ src/server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const html = require('./html').default;
+const fetch = require('node-fetch');
+
+Object.assign(global, {
+  fetch,
+  Headers: fetch.Headers,
+});
+
+const app = express();
+const port = 8000;
+
+app.use(express.static('./build/client'));
+
+app.use(html);
+
+app.listen(port, () => {
+  console.log(`listening on ${port}...`);
+});
