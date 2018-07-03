5addb13c13c93434441105077431658a382794e0
Ricardo Mayerhofer <ricardo_mayerhofer@epam.com>
2018-07-03 15:13:40 +0200

    Fix NavHeader not known component.

--- src/client/components/AppHeader.js
+++ src/client/components/AppHeader.js
@@ -10,6 +10,14 @@ import * as pageTexts from '../../constants/pageTexts';
 //     </Nav>
 // );
 
+const NavHeader = (navabarBrandStyle, linkStyle) => (
+    <Navbar.Header>
+        <Navbar.Brand style={navabarBrandStyle}>
+            <h3><a style={linkStyle} href="#home">{pageTexts.CAPTION_HEADER_TITLE}</a></h3>
+        </Navbar.Brand>
+    </Navbar.Header>
+);
+
 class AppHeader extends React.Component {
     navbarStyle = {
         background: 'black',
@@ -25,18 +33,10 @@ class AppHeader extends React.Component {
 
     navabarBrandStyle = { backgroundColor: 'black', marginLeft: '100px!important' }
 
-    NavHeader = () => (
-        <Navbar.Header>
-            <Navbar.Brand style={this.navabarBrandStyle}>
-                <h3><a style={this.linkStyle} href="#home">{pageTexts.CAPTION_HEADER_TITLE}</a></h3>
-            </Navbar.Brand>
-        </Navbar.Header>
-    );
-
     render() {
         return (
             <Navbar style={this.navbarStyle}>
-                <NavHeader />
+                <NavHeader navabarBrandStyle={this.navabarBrandStyle} linkStyle={this.linkStyle} />
                 {/* <NavBody content={this.props.content} /> */}
             </Navbar>);
     }
